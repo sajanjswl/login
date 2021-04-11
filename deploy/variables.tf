71e3a09c4a9553a9d6a38fc788b586567c0ec449
Sajan Jaiswal <sjnjaiswal@gmail.com>
2021-04-11 18:14:11 +0530

    Added AWS ECS Task Defination

--- deploy/variables.tf
+++ deploy/variables.tf
@@ -24,3 +24,19 @@ variable "bastion_key_name" {
   default = "recipe-app-api-devops-bastion"
 }
 
+
+variable "ecr_image_api" {
+  description = "ECR Image for API"
+  default     = "PP ECR Image URL:latest"
+}
+
+variable "ecr_image_proxy" {
+  description = "ECR Image for API"
+  default     = "App ECR Image for Proxy:latest"
+}
+
+variable "django_secret_key" {
+  description = "Secret key for Django app"
+}
+
+
