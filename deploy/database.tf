a9b6217ee523fedc63f3097a3f5d176b862e4002
Sajan Jaiswal <sjnjaiswal@gmail.com>
2021-04-11 22:20:43 +0530

    Added AWS ECS Service Api

--- deploy/database.tf
+++ deploy/database.tf
@@ -21,7 +21,8 @@ resource "aws_security_group" "rds" {
     from_port = 5432
     to_port   = 5432
     security_groups = [
-      aws_security_group.bastion.id
+      aws_security_group.bastion.id,
+      aws_security_group.ecs_service.id,
     ]
 
   }
